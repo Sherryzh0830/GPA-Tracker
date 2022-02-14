bee618628cdaf5d775d0e2e680a2b13e0d9fcb9b
Sherry <sherryzh0830@gmail.com>
2022-02-14 15:38:04 +0800

    multiple page

---
+++ src/InputPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function InputPage(){
+    return (
+        <div>
+            <h1>
+                Input Page Goes Here
+            </h1>
+        </div>
+    )
+}
