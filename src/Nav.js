76f43bad6eab79f7ad29925293d7c1ecf1742bb0
Sherry <sherryzh0830@gmail.com>
2022-02-21 11:17:39 +0800

    BrowserRouter

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 export default function Nav() {
   return (
     <nav>
       <h3>Logo</h3>
       <ul className="nav-links">
-        <li>Home</li>
-        <li>Courses</li>
-        <li>Add New</li>
+        <Link to="/" >Home</Link>
+        <Link to="/Main" >Courses</Link>
+        <Link to="/InputPage" >Add New</Link>
       </ul>
     </nav>
   );
