5ab37744b4315f21346e509f60c1f5e90a5b9507
Sherry <sherryzh0830@gmail.com>
2022-02-21 11:16:54 +0800

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import Nav from "./Nav";
 import Home from "./element/Home";
 import Main from "./element/Main";
 import InputPage from "./element/InputPage";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Nav />
-      <main>
-      
-          <Home />
-        
-          <Main />
-        
-          <InputPage />
-
-      </main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Main' element={<Main />} />
+            <Route path='/InputPage' element={<InputPage />}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
