138c43e8c7f056cf3eef957003adb53f3e2125c5
Sherry <sherryzh0830@gmail.com>
2022-03-02 11:50:51 +0800

    favicon and input page

--- client/src/element/Home.js
+++ client/src/element/Home.js
@@ -1,38 +1,7 @@
 import React, { useState } from "react";
-import Axios from "axios";
-import "./home.css";
 
 
 export default function Home() {
-  const [ListofEvent, setListofEvent] = useState([]);
-  const [component, setComponent] = useState("");
-  const [weight, setWeight] = useState(0);
-  const [grade, setGrade] = useState(0);
-
-  const AddEvent = () =>{
-    Axios.post("http://localhost:3001/createEvent",{component:component, weight:weight, grade:grade})
-    .then(()=>{
-      setListofEvent([...ListofEvent,{component:component, weight:weight, grade:grade}])
-    })
-  }
-
-  const UpdateEvent =(id) =>{
-    const newComponent = prompt("Please enter new component")
-    Axios.post("http://localhost:3001/updateEvent",{newComponent:newComponent, id:id})
-    .then(()=>{
-      setListofEvent(ListofEvent.map((val)=>{
-        return val.id === id ? {_id:id, component:val.component} : val;
-      }))
-    })
-  }
-
-  const DeleteEvent = (id) => {
-    Axios.delete(`http://localhost:3001/deleteEvent/${id}`)
-  }
-
-  Axios.get("http://localhost:3001/getEvent").then((response) => {
-    setListofEvent(response.data);
-  });
 
   return (
     <div>
@@ -40,7 +9,6 @@ export default function Home() {
 
       <header className="App-header">
         <h1>Welcome to the GPA Tracker!</h1>
-        <button>Sign In Here</button>
       </header>
 
       <h3 className="App-text">
@@ -49,41 +17,7 @@ export default function Home() {
       </h3>
       <b>Have fun!</b>
       
-      <div>
-      <table className="component">
-      <thead>
-        <tr>
-          <th>Component</th>
-          <th>Weight</th>
-          <th>Grade</th>
-          <th>Edit</th>
-        </tr>
-      </thead>
-      <tbody>
-      {ListofEvent.map((val) => {
-        return(
-         <tr>
-          <td>{val.component}</td>
-          <td>{val.weight}%</td>
-          <td>{val.grade}</td>
-          <div className="buttons">
-          <button onClick={()=>UpdateEvent(val._id)}>Update</button>
-          <button onClick={()=>DeleteEvent(val._id)}>Delete</button>
-          </div>
-        </tr>
-      )})}
-      </tbody>
-      </table>
-        
-      <div>
-        <h4>Add more</h4>
-        <input type="text" onChange={(event)=>(setComponent(event.target.value))} value={component} placeholder="Please enter component" />
-        <input type="number" onChange={(event)=>(setWeight(event.target.value))} value={weight} placeholder="Please enter weight" />
-        <input type="number" onChange={(event)=>(setGrade(event.target.value))} value={grade} placeholder="Please enter grade" />
-        <button onClick={AddEvent}>Add</button>
-      </div>
-
-      </div>
+      
 
       {/*<div>
         {ListofEvent.map((val) => {
--- client/src/element/InputPage.js
+++ client/src/element/InputPage.js
@@ -1,10 +1,80 @@
 import React, { useState } from "react";
+import Axios from "axios";
+import "./InputPage.css";
+
 
 export default function InputPage() {
+  const [ListofEvent, setListofEvent] = useState([]);
+  const [component, setComponent] = useState("");
+  const [weight, setWeight] = useState(0);
+  const [grade, setGrade] = useState(0);
+
+  const AddEvent = () =>{
+    Axios.post("http://localhost:3001/createEvent",{component:component, weight:weight, grade:grade})
+    .then(()=>{
+      setListofEvent([...ListofEvent,{component:component, weight:weight, grade:grade}])
+    })
+  }
+
+  const UpdateEvent =(id) =>{
+    const newComponent = prompt("Please enter new component")
+    Axios.put("http://localhost:3001/updateEvent",{newComponent:newComponent, id:id})
+    .then(()=>{
+      setListofEvent(ListofEvent.map((val)=>{
+        return val.id === id ? {_id:id, component:val.component} : val;
+      }))
+    })
+  }
+
+  const DeleteEvent = (id) => {
+    Axios.delete(`http://localhost:3001/deleteEvent/${id}`)
+  }
+
+  Axios.get("http://localhost:3001/getEvent").then((response) => {
+    setListofEvent(response.data);
+  });
+
 
   return (
     <div>
       <h1>Input Page Goes Here</h1>
+
+      <div>
+      <table className="component">
+      <thead>
+        <tr>
+          <th>Component</th>
+          <th>Weight</th>
+          <th>Grade</th>
+          <th>Edit</th>
+        </tr>
+      </thead>
+      <tbody>
+      {ListofEvent.map((val) => {
+        return(
+         <tr>
+          <td>{val.component}</td>
+          <td>{val.weight}%</td>
+          <td>{val.grade}</td>
+          <div className="buttons">
+          <button onClick={()=>UpdateEvent(val._id)}>Update</button>
+          <button onClick={()=>DeleteEvent(val._id)}>Delete</button>
+          </div>
+        </tr>
+      )})}
+      </tbody>
+      </table>
+        
+      <div>
+        <h4>Add more</h4>
+        <input type="text" onChange={(event)=>(setComponent(event.target.value))} value={component} placeholder="Please enter component" />
+        <input type="number" onChange={(event)=>(setWeight(event.target.value))} value={weight} placeholder="Please enter weight" />
+        <input type="number" onChange={(event)=>(setGrade(event.target.value))} value={grade} placeholder="Please enter grade" />
+        <button onClick={AddEvent}>Add</button>
+      </div>
+
+      </div>
+
     </div>
   );
 }
